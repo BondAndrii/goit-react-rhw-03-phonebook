40cddc8935be53fd1453ec27d99a535eb3ff7b96
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2023-05-02 11:02:00 +0300

    write to localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,12 +13,13 @@ import { Filter } from "components/Filter/Filter";
 export class App extends Component {
   
   state = {
-   contacts: [
+    contacts: [],
+      //[
     // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
     // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
     // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
     // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
+  //],
     filter:'',
     
   }
@@ -31,9 +32,11 @@ export class App extends Component {
     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
   ]
-  const newContacts = JSON.stringify(contacts);
+    const newContacts = JSON.stringify(contacts);
+    localStorage.setItem("newContacts", newContacts);
+
     try {
-      this.setState({ contacts: JSON.parse(newContacts) })
+      this.setState({ contacts: JSON.parse(localStorage.getItem("newContacts")) })
     } catch (error) {
       console.log(error.name);
       console.log(error.message)
